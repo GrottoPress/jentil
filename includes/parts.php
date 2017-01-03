120467333431367abf1a1e27bfac8079a677df47
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2017-01-03 07:57:15 +0000

    check if colophon not empty before returning part markup

--- includes/parts.php
+++ includes/parts.php
@@ -31,11 +31,11 @@ class Parts {
 	 *
 	 * @since       MagPack 0.1.0
 	 * @access      private
-	 * 
+	 *
 	 * @var         \GrottoPress\Jentil\Template\Template         $template       Template
 	 */
 	private $template;
-	
+
 	/**
 	 * Constructor
 	 *
@@ -45,23 +45,23 @@ class Parts {
 	public function __construct() {
 	    $this->template = new \GrottoPress\Jentil\Template\Template();
 	}
-    
+
     /**
      * Microdata schema
-     * 
+     *
 	 * Use schema.org's vocabulary to provide microdata
 	 * markup for this theme.
-	 * 
+	 *
 	 * @see         http://www.paulund.co.uk/add-schema-org-wordpress
 	 *
 	 * @since       Jentil 0.1.0
 	 * @access      public
-	 * 
+	 *
 	 * @filter      language_attributes
 	 */
     public function html_microdata( $output ) {
     	$output .= ' itemscope itemtype="http://schema.org/';
-        
+
         if ( $this->template->is( 'author' ) ) {
             $output .= 'ProfilePage';
         } elseif ( $this->template->is( 'search' ) ) {
@@ -69,20 +69,20 @@ class Parts {
         } else {
             $output .= 'WebPage';
         }
-        
+
     	$output .= '" ';
-    
+
     	return $output;
     }
-    
+
     /**
      * Search form
      *
      * @see 		https://developers.google.com/structured-data/slsb-overview
-     * 
+     *
      * @since       Jentil 0.1.0
 	 * @access      public
-	 * 
+	 *
 	 * @filter      get_search_form
      */
     public function search_form( $searchform ) {
@@ -92,54 +92,54 @@ class Parts {
     			<meta id="meta-search-target" itemprop="target" content="' . esc_attr( home_url( '/' ) ) . '?s={s}" />
     			<label class="screen-reader-text" for="s">' . esc_html__( 'Search for:', 'jentil' ) . '</label>
     			<input itemprop="query-input" type="search" placeholder="' . esc_attr__( 'Search', 'jentil' ) . '" class="input search" name="s" id="s" value="';
-    			   
+
     				if ( get_search_query() ) {
     				    $searchform .= esc_attr( get_search_query() );
     				}
-    				
+
     				$searchform .= '" required />
                     <input type="submit" class="input submit" value="' . esc_attr__( 'Search', 'jentil' ) . '" />
     		</form>
     	</div>';
-        
+
     	return $searchform;
     }
-    
+
     /**
      * Header logo
-     * 
+     *
      * @todo        Fill in the img src with logo theme mod from
      *              customizer and custom logo since WP 4.6
-     * 
+     *
      * @since       Jentil 0.1.0
 	 * @access      public
-	 * 
+	 *
 	 * @action      jentil_inside_header
      */
     public function header_logo() {
     	$logo = new Logo();
-    	
+
     	echo $logo->get();
     }
-    
+
     /**
      * Header search
-     * 
+     *
      * @since       Jentil 0.1.0
 	 * @access      public
-	 * 
+	 *
 	 * @action      jentil_inside_header
      */
     public function header_search() {
     	get_search_form();
     }
-    
+
     /**
      * Header menu
-     * 
+     *
      * @since       Jentil 0.1.0
 	 * @access      public
-	 * 
+	 *
 	 * @action      jentil_inside_header
      */
     public function header_menu() {
@@ -147,29 +147,29 @@ class Parts {
     	    <div class="screen-reader-text skip-link">
     	        <a href="#content">' . esc_html__( 'Skip to content', 'jentil' ) . '</a>
             </div>';
-            
+
             wp_nav_menu( array( 'theme_location' => 'primary-menu' ) );
-            
+
         echo '</nav>';
     }
-    
+
     /**
      * Mobile header menu
-     * 
+     *
      * @since       Jentil 0.1.0
 	 * @access      public
-	 * 
+	 *
 	 * @action      jentil_inside_header
      */
     public function mobile_header_menu() {
     	$status = isset( $_GET['menu'] ) ? sanitize_key( $_GET['menu'] ) : 'off';
     	$pagination = new \GrottoPress\MagPack\Pagination();
-    	
+
     	echo '<div class="site-navigation main-navigation menu-max-screen-920">
     	    <div class="screen-reader-text skip-link">
     	        <a href="#content">' . esc_html__( 'Skip to content', 'jentil' ) . '</a>
             </div>
-            
+
             <div class="menu-mobile-menu-container">
     	        <ul class="menu mobile-menu">
     	            <li class="menu-item hamburger">
@@ -180,75 +180,75 @@ class Parts {
     	        </ul>
             </div>
         </div>
-    	
+
     	<nav role="navigation" class="menu-max-screen-920" ' . ( $status == 'off' ? ' style="display:none;"' : '' ) . '>';
-            
+
             get_search_form(); wp_nav_menu( array( 'theme_location' => 'primary-menu' ) );
-            
+
         echo '</nav>';
     }
-    
+
     /**
      * Breadcrumbs
-     * 
+     *
      * @since       Jentil 0.1.0
 	 * @access      public
-	 * 
+	 *
 	 * @action      jentil_before_title
      */
     public function breadcrumbs() {
     	if ( $this->template->is( 'front_page' ) && ! is_paged() ) {
     	    return;
     	}
-    
+
         $args = array(
             'before' => esc_html__( 'Path: ', 'jentil' ),
         );
-    	
+
     	$breadcrumbs = new \GrottoPress\MagPack\Breadcrumbs( $args );
     	echo $breadcrumbs->trail();
     }
-    
+
     /**
      * Single post entry meta
-     * 
+     *
      * @since       Jentil 0.1.0
 	 * @access      public
-	 * 
+	 *
 	 * @action      jentil_after_title
      */
     public function single_post_entry_meta() {
     	if ( ! $this->template->is( 'singular', 'post' ) ) {
     	    return;
     	}
-    	
+
     	global $post;
-    	
+
     	$magpack_post = new \GrottoPress\MagPack\Post\Post( $post->ID );
     	$avatar = $magpack_post->meta_list( 'avatar__40', '' );
     	$author = $magpack_post->meta_list( 'author_link', '' );
-    	
+
     	$output = '<div class="entry-meta after-title self-clear">';
-    	
+
     	if ( ! empty( $avatar ) ) {
     	    $output .= $avatar;
     	}
-    	
+
     	if ( ! empty( $author ) ) {
     	    $output .= '<p>' . $author . '</p>';
     	}
-    	
+
     	$output .= '<p>' . $magpack_post->meta_list( 'published_date, published_time, comments_link' ) . '</p></div>';
-        
+
         echo $output;
     }
-    
+
     /**
      * Footer Widget Area.
      *
      * @since       Jentil 0.1.0
      * @access      public
-     * 
+     *
      * @action      jentil_inside_footer
      */
     public function footer_widgets() {
@@ -258,39 +258,43 @@ class Parts {
 			</div><!-- .widget-area -->
 		<?php }
     }
-    
+
     /**
      * Colophon
-     * 
+     *
      * @since       Jentil 0.1.0
      * @access      public
-     * 
+     *
      * @action      jentil_inside_footer
      */
     public function colophon() {
     	$colophon = new Colophon();
-    	
-    	echo '<div id="colophon"><small>' . $colophon->get() . '</small></div><!-- #colophon -->';
+
+        if ( ! ( $get = $colophon->get() ) ) {
+            return '';
+        }
+
+    	echo '<div id="colophon"><small>' . $get . '</small></div><!-- #colophon -->';
     }
-    
+
     /**
      * Body class
-     * 
+     *
      * Add classes to <body>
-     * 
+     *
      * @since       Jentil 0.1.0
      * @access      public
-     * 
+     *
      * @filter      body_class
      */
     public function body_class( $classes ) {
         global $post; //, $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
-    	
+
     	if ( $this->template->is( 'singular' ) ) {
             if ( is_post_type_hierarchical( $post->post_type ) ) {
                 if ( ! empty( $post->post_parent ) ) {
         			$parent_id = $post->post_parent;
-        			
+
         			while ( $parent_id ) {
         				$page = get_post( $parent_id );
         				$classes[] = sanitize_title( $post_type . '-parent-' . $page->ID );
@@ -298,40 +302,40 @@ class Parts {
         			}
         		}
     	    }
-    	    
+
     	    $template = get_page_template_slug( $post->ID );
-    	    
+
     	    if ( ! empty( $template ) ) {
     			$classes[] = sanitize_title( $template );
     		}
-    	    
+
     	    if ( post_type_supports( $post->post_type, 'comments' ) ) {
         	    $classes[] = get_option( 'show_avatars' ) ? 'show-avatars' : 'hide-avatars';
         	    $classes[] = get_option( 'thread_comments' ) ? 'threaded-comments' : 'unthreaded-comments';
         	    $classes[] = comments_open( $post->ID ) ? 'comments-open' : 'comments-closed';
     	    }
     	}
-    	
+
     	$template = new \GrottoPress\Jentil\Template\Template();
         $layout = $template->layout()->get();
         $layout_column = $template->layout()->column();
-    	
+
     	if ( ! empty( $layout ) ) {
     		$classes[] = sanitize_title( 'layout-' . $layout );
     		$classes[] = sanitize_title( 'layout-' . $layout_column );
     	}
-    	
+
     	return $classes;
     }
-    
+
     /**
      * Dynamic styles
-     * 
+     *
      * Add dynamic styles to theme
      *
      * @since       Jentil 0.1.0
      * @access      public
-     * 
+     *
      * @action      wp_head
      */
     public function dynamic_styles() {
