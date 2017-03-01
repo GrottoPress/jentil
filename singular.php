2199d1b73f24bbfb5f811a249c361ceeab3235fb
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2017-03-01 22:17:53 +0000

    removed page.php and single.php in favour of singular.php

---
+++ singular.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Default page template
+ * 
+ * @see 			http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @link			https://jentil.grottopress.com
+ * @package			jentil
+ * @since			Jentil 0.1.0
+ */
+
+get_header();
+
+?>
+
+<div id="container">
+	<main id="content">
+		
+		<?php
+		/**
+		 * Do action before title
+		 * 
+		 * @action		jentil_before_title
+		 *
+		 * @since       Jentil 0.1.0
+		 */
+		do_action( 'jentil_before_title' );
+		
+		the_post();
+
+		get_template_part( 'parts/content', 'singular' );
+
+		rewind_posts();
+
+		/**
+		 * Do action after content
+		 * 
+		 * @action		jentil_after_content
+		 *
+		 * @since       Jentil 0.1.0
+		 */
+		do_action( 'jentil_after_content' );
+		
+		the_post();
+		
+		if ( 'open' == get_option( 'default_ping_status' ) ) {
+			echo '<!--'; trackback_rdf(); echo '-->';
+		}
+		
+		comments_template( '', true );
+		
+		rewind_posts(); ?>
+		
+	</main><!-- #content -->
+</div><!-- #container -->
+
+<?php
+
+get_sidebar();
+
+get_footer();
