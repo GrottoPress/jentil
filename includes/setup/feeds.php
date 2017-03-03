fc2ca9775373c4b4061b059d74b5ba36384eae20
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2017-03-03 23:23:24 +0000

    added feeds setup

---
+++ includes/setup/feeds.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Feeds (Atom, RSS etc)
+ *
+ * @link            https://jentil.grotttopress.com
+ * @package		    jentil
+ * @subpackage 	    jentil/includes
+ * @since		    Jentil 0.1.0
+ */
+
+namespace GrottoPress\Jentil\Setup;
+
+if ( ! defined( 'WPINC' ) ) {
+    wp_die( esc_html__( 'Do not load this file directly!', 'jentil' ) );
+}
+
+use GrottoPress\MagPack;
+
+/**
+ * Feeds (Atom, RSS etc)
+ *
+ * @link			https://jentil.grotttopress.com
+ * @package			jentil
+ * @subpackage 	    jentil/includes
+ * @since			jentil 0.1.0
+ */
+final class Feeds extends MagPack\Utilities\Singleton {
+    /**
+	 * Constructor
+	 *
+	 * @since       MagPack 0.1.0
+	 * @access      public
+	 */
+	protected function __construct() {}
+
+    /**
+     * Add support for RSS and atom feeds
+     * 
+     * @since       jentil 0.1.0
+     * @access      public
+     * 
+     * @action      after_setup_theme
+     */
+    public function enable() {
+        add_theme_support( 'automatic-feed-links' );
+    }
+}
