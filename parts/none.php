17f7af7fe137d1f282197487ab0e96878b2784a8
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2017-07-08 23:54:40 +0000

    added api functions for child themes and plugins

--- parts/none.php
+++ parts/none.php
@@ -12,8 +12,6 @@ if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
-use GrottoPress\Jentil\Utilities;
-
 ?>
 
 <div class="posts-wrap">
@@ -34,7 +32,7 @@ use GrottoPress\Jentil\Utilities;
 				'<h2 class="entry-title" itemprop="name headline">' . esc_html__( 'Nothing Found', 'jentil' ) . '</h2>'
 				
 				. '<p>' . esc_html__( 'Sorry, nothing here ):', 'jentil' ) . '</p>',
-				Utilities\Template\Template::instance()->type()
+				jentil_template()->type()
 			);
 			
 			echo $jentil_nothing_found_content;
