03989c60d02e7e5e1ab6d47f9a5c596515f516d6
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2017-10-09 01:19:05 +0000

    improve menu js to close all open submenus before opening current submenu

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -6,52 +6,79 @@
  * @since 0.1.0
  */
 
-(function($) {
+(function ($) {
     'use strict';
 
-    // Make the mobile menu button work
+    // Mobile menu button
     $('.js-mobile-menu').hide();
     $('.js-mobile-menu-button').attr('href', '#');
-    $('.js-mobile-menu-button').on('click', function(event) {
+    $('.js-mobile-menu-button').on('click', function (e) {
         $('.js-mobile-menu').slideToggle({
             'duration': 200
         });
 
-        event.preventDefault();
+        e.preventDefault();
     });
 
     // Add icons to all parent menu items
-    // Source: http://stackoverflow.com/questions/6752677/use-jquery-to-automatically-add-arrows-to-all-parent-menus
-    $('.menu li > ul').before('<button class="js-sub-menu-button sub-menu-toggle closed"><span class="fa fa-caret-down" aria-hidden="true"></span><span class="screen-reader-text">Sub-menu</span></button>');
+    $('.menu li > ul').before('<button class="js-sub-menu-button sub-menu-toggle closed">'+renderCaret('down')+'</button>');
 
-    // Sub-menus toggle
+    // Sub-menu button
     $('.js-sub-menu-button').next('ul').hide();
-    $('.js-sub-menu-button').prev('a').on('click', function(event) {
-        if ('#' == $(this).attr('href')) {
-            subMenuToggle($(this).next('button'));
+    $('.js-sub-menu-button').prev('a').on('click', function (e) {
+        if ('#' === $(this).attr('href')) {
+            toggleSubMenu($(this).next('button'));
 
-            event.preventDefault();
+            e.preventDefault();
         }
     });
-    $('.js-sub-menu-button').on('click', function(event) {
-        subMenuToggle(this);
+    $('.js-sub-menu-button').on('click', function (e) {
+        toggleSubMenu(this);
 
-        event.preventDefault();
+        e.preventDefault();
     });
 
     // Toggle Submenu
-    function subMenuToggle(selector) {
-        var html = $(selector).html();
-
-        if (html.indexOf('fa-caret-up') >= 0) {
-            $(selector).html('<span class="fa fa-caret-down" aria-hidden="true"></span><span class="screen-reader-text">Sub-menu</span>');
-        } else {
-            $(selector).html('<span class="fa fa-caret-up" aria-hidden="true"></span><span class="screen-reader-text">Sub-menu</span>');
-        }
-
+    function toggleSubMenu(selector)
+    {
         $(selector).toggleClass('closed');
+
+        $(selector).parent().siblings('li').children('ul').hide();
+        toggleCaretAlt($(selector).parent().siblings('li').children('button'));
+
         $(selector).next('ul').slideToggle({
             'duration': 200
         }); // override `display:none;` in CSS for hover
+        toggleCaret(selector);
+    }
+
+    // Toggle Caret
+    function toggleCaret(selector)
+    {
+        var html = $(selector).html();
+
+        if (html.indexOf('fa-caret-up') >= 0) {
+            $(selector).html(renderCaret('down'));
+        } else {
+            $(selector).html(renderCaret('up'));
+        }
+    }
+
+    // Toggle Caret
+    function toggleCaretAlt(selector)
+    {
+        if ('none' === $(selector).next('ul').css('display')) {
+            $(selector).html(renderCaret('down'));
+        } else {
+            $(selector).html(renderCaret('up'));
+        }
+    }
+
+    // Up/Down button HTML
+    function renderCaret(direction)
+    {
+        return '<span class="fa fa-caret-'+direction.toString().toLowerCase()+
+            '" aria-hidden="true"></span>'+
+            '<span class="screen-reader-text">Sub-menu</span>';
     }
 })(jQuery);
