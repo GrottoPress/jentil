7dc187da4de13f5b31435b97841ff4271c2493ac
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-03-11 21:45:02 +0000

    Remove redundant doc block comments

--- functions.php
+++ functions.php
@@ -1,32 +1,12 @@
 <?php
-
-/**
- * Functions
- *
- * @package GrottoPress\Jentil
- * @since 0.1.0
- *
- * @see https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @author GrottoPress <info@grottopress.com>
- * @author N Atta Kusi Adusei
- */
-
 declare (strict_types = 1);
 
-/**
- * Autoloader
- *
- * @since 0.1.0
- */
 require __DIR__.'/vendor/autoload.php';
 
 /**
- * Run this theme.
+ * Run theme
  *
  * @action after_setup_theme
- *
- * @since 0.1.0
  */
 \add_action('after_setup_theme', function () {
     \Jentil()->run();
