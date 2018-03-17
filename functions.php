1d98bb9cc8c194c1acde026e79a751492d5a7f42
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-03-17 11:53:47 +0000

    Remove redundant comments

--- functions.php
+++ functions.php
@@ -3,11 +3,6 @@ declare (strict_types = 1);
 
 require __DIR__.'/vendor/autoload.php';
 
-/**
- * Run theme
- *
- * @action after_setup_theme
- */
 \add_action('after_setup_theme', function () {
     \Jentil()->run();
 }, 0);
