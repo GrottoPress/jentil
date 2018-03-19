977ba87e6caa185c958466c7b521d72cb19f75f3
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-03-19 16:50:49 +0000

    Update default colophon mod
    It's now "(c) <year> <site name>. Built with Jentil on WordPress".

--- app/libraries/Utilities/ThemeMods/Colophon.php
+++ app/libraries/Utilities/ThemeMods/Colophon.php
@@ -20,12 +20,13 @@ class Colophon extends AbstractThemeMod
 
         $this->default = \sprintf(
             \esc_html__(
-                '&copy; %1$s %2$s. Built with %3$s',
+                '&copy; %1$s %2$s. Built with %3$s on %4$s',
                 'jentil'
             ),
             '<span itemprop="copyrightYear">{{this_year}}</span>',
             '<a class="blog-name" itemprop="url" href="{{site_url}}"><span itemprop="copyrightHolder">{{site_name}}</span></a>',
-            '<em><a itemprop="url" rel="nofollow" href="'.Jentil::WEBSITE.'">'.Jentil::NAME.'</a></em>.'
+            '<em><a itemprop="url" rel="nofollow" href="'.Jentil::WEBSITE.'">'.Jentil::NAME.'</a></em>',
+            '<a itemprop="url" rel="nofollow" href="https://wordpress.org">WordPress</a>.'
         );
     }
 
