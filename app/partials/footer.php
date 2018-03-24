259a7006c691c361b2861dc07b59ea3daae01421
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-03-24 16:47:09 +0000

    Remove trackback rdf
    Haven't seen it used any where in a while, not even in the
    twenty-sixteen and twenty-seventeen themes.

--- app/partials/footer.php
+++ app/partials/footer.php
@@ -3,18 +3,9 @@ declare (strict_types = 1);
 
         \do_action('jentil_after_after_content');
 
-        /**
-         * Load comments
-         */
         if (\Jentil()->utilities->page->is('singular')) {
-            if ('open' === \get_option('default_ping_status')) {
-                echo '<!--';
-                \trackback_rdf();
-                echo '-->';
-            }
-
             \Jentil()->utilities->loader->loadComments();
-        } ?>
+         } ?>
     </main><!-- #content -->
 </div><!-- #content-wrap -->
 
