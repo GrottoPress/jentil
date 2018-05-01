4191092edd7e27c31c75d5f63a99f0bf4f491e5e
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-05-01 12:53:11 +0000

    Move `||` at the beginning of lines to end of previous line

--- app/templates/index.php
+++ app/templates/index.php
@@ -4,8 +4,9 @@ declare (strict_types = 1);
 \Jentil()->utilities->loader->loadPartial('header');
 
 if (!\Jentil()->utilities->page->is('singular')) {
-    if (($jentil_title = \Jentil()->utilities->page->title->themeMod()->get())
-        || \Jentil()->utilities->page->is('customize_preview')
+    if (($jentil_title =
+        \Jentil()->utilities->page->title->themeMod()->get()) ||
+        \Jentil()->utilities->page->is('customize_preview')
     ) { ?>
         <header class="page-header">
             <?php \do_action('jentil_before_title'); ?>
@@ -26,8 +27,8 @@ unset($jentil_title);
 
 \do_action('jentil_before_content');
 
-if (\Jentil()->utilities->page->is('404')
-    || !($jentil_posts = \Jentil()->utilities->page->posts->render())
+if (\Jentil()->utilities->page->is('404') ||
+    !($jentil_posts = \Jentil()->utilities->page->posts->render())
 ) {
     \Jentil()->utilities->loader->loadPartial('none');
 } else {
