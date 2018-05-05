1338c07fc36f1a39ff16f8c03917072134893938
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-05-05 20:31:32 +0000

    Replace native loops with jQuery loop

--- assets/scripts/customize-preview.ts
+++ assets/scripts/customize-preview.ts
@@ -15,43 +15,43 @@
     /**
      * Page Title
      */
-    for (let i in jentilPageTitleModIds) {
-        wp.customize(jentilPageTitleModIds[i], (value: () => void): void => {
+    $.each(jentilPageTitleModIds, (i: number, id: string): void => {
+        wp.customize(id, (value: () => void): void => {
             value.bind((to: string): void => {
                 $('.page-title').html(replaceShortTags(to))
             })
         })
-    }
+    })
 
     /**
      * Posts heading
      */
-    for (let i in jentilRelatedPostsHeadingModIds) {
-        wp.customize(jentilRelatedPostsHeadingModIds[i], (value: () => void): void => {
+    $.each(jentilRelatedPostsHeadingModIds, (i: number, id: string): void => {
+        wp.customize(id, (value: () => void): void => {
             value.bind((to: string): void => {
                 $('#related-posts-wrap .posts-heading').html(to)
             })
         })
-    }
+    })
 
     /**
      * Layout
      */
-    for (let i in jentilPageLayoutModIds) {
-        wp.customize(jentilPageLayoutModIds[i], (value: () => void): void => {
+    $.each(jentilPageLayoutModIds, (i: number, id: string): void => {
+        wp.customize(id, (value: () => void): void => {
             value.bind((to: string): void => {
                 $('body').attr('class', (i: number, c: string): string =>
                     c.replace(/(^|\s)layout\-\S+/g, '')
                 ).addClass(`layout-${to} layout-columns-${to.split('-').length}`)
             })
         })
-    }
+    })
 
     function replaceShortTags(content: string): string
     {
-        for (let tag in jentilShortTags) {
-            content = content.split(tag).join(jentilShortTags[tag])
-        }
+        $.each(jentilShortTags, (tag: string, replace: string): void => {
+            content = content.split(tag).join(replace)
+        })
 
         return content
     }
