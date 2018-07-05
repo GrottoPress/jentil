f300e7abeaeddb6f538a0c5d83fe19f86fe2928d
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-07-05 23:59:49 +0000

    Deactivate theme if min required WP and PHP versions not met

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,7 +6,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## Unreleased 0.8.0 - 
 
-## Changed
+### Added
+- Deactivate theme if minimum required WordPress and PHP versions not met
+
+### Changed
 - Move `isPagelike()` from posts utility to layout utility.
 
 ## 0.7.1 - 2018-06-24
--- functions.php
+++ functions.php
@@ -1,8 +1,54 @@
 <?php
-declare (strict_types = 1);
 
-require __DIR__.'/vendor/autoload.php';
+/**
+ * NOTE: Keep code in this file compatible with PHP 5.2
+ */
 
-\add_action('after_setup_theme', function () {
-    \Jentil()->run();
-}, 0);
+define('MINIMUM_PHP', '7.0');
+define('MINIMUM_WP', '4.7');
+
+if (version_compare(PHP_VERSION, MINIMUM_PHP, '<') ||
+    version_compare(get_bloginfo('version'), MINIMUM_WP, '<')
+) {
+    add_action('admin_notices', 'printJentilReqNotice');
+
+    deactivateJentil();
+} else {
+    require __DIR__.'/vendor/autoload.php';
+
+    add_action('after_setup_theme', 'runJentil', 0);
+}
+
+function runJentil()
+{
+    Jentil()->run();
+}
+
+function printJentilReqNotice()
+{
+    echo '<div class="notice notice-error">
+        <p>'.
+        sprintf(
+            __(
+                '%1$s theme has been deactivated as it requires PHP >= %2$s and WordPress >= %3$s'
+            ),
+            '<code>jentil</code>',
+            '<strong>'.MINIMUM_PHP.'</strong>',
+            '<strong>'.MINIMUM_WP.'</strong>'
+        ).
+        '</p>
+    </div>';
+}
+
+function deactivateJentil()
+{
+    $themes = wp_get_themes(['allowed' => true]);
+    unset($themes['jentil']);
+
+    $theme = reset($themes);
+    $name = null === key($themes) ? '' : $theme->get_stylesheet();
+    $parent = $name ? $theme->get_template() : '';
+
+    update_option('stylesheet', $name);
+    update_option('template', $parent);
+}
