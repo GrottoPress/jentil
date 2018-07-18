103bba55b263f21732fd6aa956249531cbfaea66
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-07-18 23:15:06 +0000

    Refactor method

--- app/libraries/Setups/Views/SearchForm.php
+++ app/libraries/Setups/Views/SearchForm.php
@@ -21,29 +21,30 @@ final class SearchForm extends AbstractSetup
     {
         $home_url = \home_url('/');
 
-        $searchform = '<div class="search-wrap" itemscope itemtype="http://schema.org/WebSite">
+        return '<div class="search-wrap" itemscope itemtype="http://schema.org/WebSite">
             <meta itemprop="url" content="'.$home_url.'"/>
 
-            <form role="search" method="get" class="form search" action="'.$home_url.'" itemprop="potentialAction" itemscope itemtype="http://schema.org/SearchAction">
+            <form role="search" method="get" class="form search" action="'.
+                $home_url.'" itemprop="potentialAction" itemscope itemtype="http://schema.org/SearchAction">
                 <meta itemprop="target" content="'.$home_url.'?s={s}" />
+
                 <label>
-                    <span class="screen-reader-text">'.\esc_html__('Search for:', 'jentil').'</span>
+                    <span class="screen-reader-text">'.
+                        \esc_html__('Search for:', 'jentil').'</span>
 
-                    <input itemprop="query-input" type="search" placeholder="'.\esc_attr__('Search', 'jentil').'" class="input search" name="s" value="';
-
-        if ($query = \get_search_query()) {
-            $searchform .= $query;
-        }
-
-        $searchform .= '" />
+                    <input itemprop="query-input" type="search" placeholder="'.
+                        \esc_attr__('Search', 'jentil').
+                        '" class="input search" name="s" value="'.
+                        (\get_search_query() ?: '').'" />
                 </label>
 
                 <button type="submit" class="button submit">
-                    <span class="fas fa-search fa-sm" aria-hidden="true"></span> <span class="search-button-text icon-text">'.\esc_html__('Search', 'jentil').'</span>
+                    <span class="fas fa-search fa-sm" aria-hidden="true"></span>
+
+                    <span class="search-button-text icon-text">'.
+                        \esc_html__('Search', 'jentil').'</span>
                 </button>
             </form>
         </div>';
-
-        return $searchform;
     }
 }
