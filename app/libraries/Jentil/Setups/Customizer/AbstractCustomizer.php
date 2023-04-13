8a32967e948150d5ec2b9d96b6ef9fa882ab33ac
akadusei <attakusiadusei@gmail.com>
2023-04-13 17:54:40 +0000

    Fix error
    ```
    [ParseError] syntax error, unexpected identifier "WordPress", expecting "{"
    
    \#1  /path/to/app/libraries/Jentil/Setups/Customizer/AbstractCustomizer.php:7
    \#2  /path/to/vendor/composer/ClassLoader.php:433
    \#3  Composer\Autoload\ClassLoader->loadClass
    \#4  /path/to/tests/unit/libraries/Jentil/Setups/Supports/WooCommerceTest.php:122
    \#5  /path/to/vendor/bin/codecept:120
    ```

--- app/libraries/Jentil/Setups/Customizer/AbstractCustomizer.php
+++ app/libraries/Jentil/Setups/Customizer/AbstractCustomizer.php
@@ -3,12 +3,8 @@ declare (strict_types = 1);
 
 namespace GrottoPress\Jentil\Setups\Customizer;
 
-use GrottoPress\
-    WordPress\
-    SUV\
-    Setups\
-    Customizer\
-    AbstractCustomizer as Customizer;
+use GrottoPress\WordPress\SUV\Setups\Customizer\AbstractCustomizer as
+    Customizer;
 
 abstract class AbstractCustomizer extends Customizer
 {
